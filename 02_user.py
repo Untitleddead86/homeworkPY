b5733d449dedcc90d64d4bf32b5476372541db10
Untitleddead86 <126862119+Untitleddead86@users.noreply.github.com>
2024-08-23 20:54:17 +0300

    opravil jsem kod
    with open("02_user.txt", "a") as file: - otevira novy file .txt
    
        file.write(jmeno + ", " + vek + "\n") - vypisuje do file.txt zmenu

---
+++ 02_user.py
@@ -0,0 +1,14 @@
+# 02_user.py
+
+"""
+2) vytvořte 02_user.py kde
+uživatel zadá své jmeno a věk pomocí input
+uložte tyto informace do souboru
+02_user.txt
+"""
+
+jmeno = input('Zadej jmeno: ')
+vek = input('Zadej vek: ')
+
+with open("02_user.txt", "a") as file:
+    file.write(jmeno + ", " + vek + "\n")
