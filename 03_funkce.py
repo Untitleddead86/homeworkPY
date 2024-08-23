ec222f2ec9d10147b1476a2ab5b1fcb60b5c3f6b
Untitleddead86 <126862119+Untitleddead86@users.noreply.github.com>
2024-08-23 20:17:58 +0200

    Create 03_funkce.py

---
+++ 03_funkce.py
@@ -0,0 +1,27 @@
+# 03_funkce.py
+
+"""
+1) vytvořte soubor 03_funkce.py
+v souboru vytvořte funkci:
+def test_heslo(heslo: str):
+
+tato funkce otestuje, zda uživatel zadal heslo alespoň o délce 5 znaků
+
+test_heslo('1234') # vrátí False
+test_heslo('12345') # vrátí True
+test_heslo('TajnéHeslo') # vrátí True
+"""
+
+heslo = input("Zadej heslo 5 a vic znaku: ")
+
+def test_heslo(heslo: str):
+    if len(heslo) >= 5:
+        return True
+    else:
+        return False
+    
+vysledek = test_heslo(heslo)
+print(vysledek)
+
+
+
